ac73f1d36b36810a05ed919bc5d89896eca1bff8
Tanner Lancaster <tanner@flatironschool.com>
2018-06-20 23:59:36 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,38 @@
 # Write your code here.
+katz_deli = []
+
+# def line(katz_deli)
+#   if katz_deli = []
+#     puts "The line is currently empty."
+#   else
+#     intro = "The line is currently:"
+#     katz_deli.each.with_index(1) do |queue, i|
+#       intro << " #{i}. #{queue}"
+#     end
+#     puts intro
+#   end
+# end
+
+def line(deli)
+  if deli.empty?
+    puts "The line is currently empty."
+  else
+    current_line = "The line is currently:"
+    deli.each.with_index(1) do |person, i|
+      current_line << " #{i}. #{person}"
+    end
+    puts current_line
+  end
+end
+
+def take_a_number(katz_deli, customer)
+
+end
+
+def now_serving()
+  puts "next person in line"
+  #if array is empty
+  #puts "There is nobody waiting to be served!"
+  # else
+  #remove person from array
+end
