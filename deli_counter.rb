0bac87d3c5515728bd4ce9844151c5c3e3ecd3d8
Tanner Lancaster <tanner@flatironschool.com>
2018-06-21 00:59:02 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -13,18 +13,6 @@ def line(deli)
   end
 end
 
-# def line(deli)
-#   if deli.empty?
-#     puts "The line is currently empty."
-#   else
-#     current_line = "The line is currently:"
-#     deli.each.with_index(1) do |person, i|
-#       current_line << " #{i}. #{person}"
-#     end
-#     puts current_line
-#   end
-# end
-
 def take_a_number(katz_deli, customer)
 
 end
