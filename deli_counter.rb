442940fbe3a83c9bfff6f4191684202509086fd7
Tanner Lancaster <tanner@flatironschool.com>
2018-06-21 01:11:19 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -20,7 +20,7 @@ def take_a_number(katz_deli, customer)
   puts "Welcome, #{most_recent_customer}. You are number #{most_recent_customer_i} in line."
 end
 
-def now_serving(katz_deli)
+def now_serving(line)
   if line.empty?
     puts "There is nobody waiting to be served!"
   else
