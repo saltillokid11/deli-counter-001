0bd977f3642c6420a9dc2a368b81eec2935bfa59
Tanner Lancaster <tanner@flatironschool.com>
2018-06-21 00:00:42 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,29 +1,29 @@
 # Write your code here.
 katz_deli = []
 
-# def line(katz_deli)
-#   if katz_deli = []
-#     puts "The line is currently empty."
-#   else
-#     intro = "The line is currently:"
-#     katz_deli.each.with_index(1) do |queue, i|
-#       intro << " #{i}. #{queue}"
-#     end
-#     puts intro
-#   end
-# end
-
-def line(deli)
-  if deli.empty?
+def line(katz_deli)
+  if katz_deli = []
     puts "The line is currently empty."
   else
-    current_line = "The line is currently:"
-    deli.each.with_index(1) do |person, i|
-      current_line << " #{i}. #{person}"
+    intro = "The line is currently:"
+    katz_deli.each.with_index(1) do |queue, i|
+      intro << " #{i}. #{queue}"
     end
-    puts current_line
+    puts intro
   end
 end
+
+# def line(deli)
+#   if deli.empty?
+#     puts "The line is currently empty."
+#   else
+#     current_line = "The line is currently:"
+#     deli.each.with_index(1) do |person, i|
+#       current_line << " #{i}. #{person}"
+#     end
+#     puts current_line
+#   end
+# end
 
 def take_a_number(katz_deli, customer)
 
